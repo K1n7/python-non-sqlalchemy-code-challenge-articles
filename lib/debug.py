01fb4b0a27f851cde3ad7699d7aa102db11c29b1
Tom Amollo <iantom96@gmail.com>
2025-05-01 21:11:27 +0300

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -8,6 +8,42 @@ from classes.many_to_many import Magazine
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+author1 = Author("Alice")
+
+author2 = Author("Bob")
+
+# Create some magazines
+mag1 = Magazine ("TechNow", "Technology")
+mag2 = Magazine("HealthPlus", "Health")
+
+# Create some articles
+
+article1 = Article(author1, mag1, "The Future of AI")
+
+article2 = Article(author1, mag2, "Healthy Living Tips")
+
+article3 = Article(author2, mag1, "AI in Everyday Life")
+
+article4 = Article(author2, mag1, "Deep Learning Explained")
+
+article5 = Article(author2, mag1, "Robots and You")
+
+
+print(author1.articles())
+
+print(author1.magazines())
+
+print(author1.topic_areas())
+
+print(mag1.articles())
+
+print(mag1.contributors())
+
+print(mag1.article_titles())
+
+print(mag1.contributing_authors())
+
+print(Magazine.top_publisher())
 
     # don't remove this line, it's for debugging!
-    ipdb.set_trace()
+ipdb.set_trace()
