ee914f2cb35d7f9f1a7a051649a439e04f92336d
Tom Amollo <iantom96@gmail.com>
2025-05-01 20:56:51 +0300

    Create author.py

---
+++ lib/author.py
@@ -0,0 +1,23 @@
+class Author:
+    def __init__(self, name):
+        if not isinstance(name, str) or len(name) == 0:
+            raise Exception("Name must be a non-empty string.")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
+
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
+
+    def add_article(self, magazine, title):
+        return Article(self, magazine, title)
+
+    def topic_areas(self):
+        if not self.articles():
+            return None
+        return list({magazine.category for magazine in self.magazines()})
